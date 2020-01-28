926c953825cceb465769673f390405f8e4530b07
Alex Netsch <alex.netsch97@gmail.com>
2020-01-28 14:59:22 +0100

    Changed relayPin

--- LightControl/light_c/light_c.ino
+++ LightControl/light_c/light_c.ino
@@ -1,5 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+
+const int relayPin = 14;
  
 const char* SSID = "FRITZ!Box 7560 BL";
 const char* PSK = "77581211526824077024";
@@ -10,14 +12,24 @@ PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
+
+
+
  
 void setup() {
-    pinMode(13, OUTPUT);
+    initRelays();
     Serial.begin(115200);
     setup_wifi();
     client.setServer(MQTT_BROKER, 1883);
     client.setCallback(callback);
 }
+
+// sets up relayPins and the res ground pin
+void initRelays(){
+  // set up the relay to be off on default for safety.
+  pinMode(relayPin,OUTPUT);
+  digitalWrite(relayPin, LOW);
+  }
  
 void setup_wifi() {
     delay(10);
@@ -37,6 +49,17 @@ void setup_wifi() {
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 }
+
+void handleMsg(char* msg){
+    if(strcmp(msg,"ON")==0){
+        Serial.println("Relay ON");
+        digitalWrite(relayPin, HIGH);
+    }
+    else if(strcmp(msg,"OFF")==0){
+       Serial.println("Relay OFF");
+        digitalWrite(relayPin, LOW);
+    }
+  }
  
 void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print("Received message [");
@@ -50,14 +73,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.println();
  
     msg[length] = '\0';
+    handleMsg(msg);
     Serial.println(msg);
  
-    if(strcmp(msg,"on")==0){
-        digitalWrite(13, HIGH);
-    }
-    else if(strcmp(msg,"off")==0){
-        digitalWrite(13, LOW);
-    }
+
 }
  
 void reconnect() {
