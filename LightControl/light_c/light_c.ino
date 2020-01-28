2ad8d6839753e67279c447b9304ef37ecd6e1033
Alex Netsch <alex.netsch97@gmail.com>
2020-01-28 13:50:47 +0100

    ESP8266 node mcu via MQTT to control a relay

---
+++ LightControl/light_c/light_c.ino
@@ -0,0 +1,82 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+const char* SSID = "FRITZ!Box 7560 BL";
+const char* PSK = "77581211526824077024";
+const char* MQTT_BROKER = "192.168.178.32";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+ 
+void setup() {
+    pinMode(13, OUTPUT);
+    Serial.begin(115200);
+    setup_wifi();
+    client.setServer(MQTT_BROKER, 1883);
+    client.setCallback(callback);
+}
+ 
+void setup_wifi() {
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+ 
+    WiFi.begin(SSID, PSK);
+ 
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+ 
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Received message [");
+    Serial.print(topic);
+    Serial.print("] ");
+    char msg[length+1];
+    for (int i = 0; i < length; i++) {
+        Serial.print((char)payload[i]);
+        msg[i] = (char)payload[i];
+    }
+    Serial.println();
+ 
+    msg[length] = '\0';
+    Serial.println(msg);
+ 
+    if(strcmp(msg,"on")==0){
+        digitalWrite(13, HIGH);
+    }
+    else if(strcmp(msg,"off")==0){
+        digitalWrite(13, LOW);
+    }
+}
+ 
+void reconnect() {
+    while (!client.connected()) {
+        Serial.println("Reconnecting MQTT...");
+        if (!client.connect("ESP8266Client")) {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" retrying in 5 seconds");
+            delay(5000);
+        }
+    }
+    client.subscribe("/home/data");
+    Serial.println("MQTT Connected...");
+}
+ 
+void loop() {
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
+}
